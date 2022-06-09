6b8e3bd23c36cce9586258b85b694f6502eb7a5a
MrSerge01 <olgadeeva61@gmail.com>
2022-06-09 09:33:38 +0600

    smol fixes
    Co-authored-by: Froxcey <danichen204@gmail.com>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,22 +5,20 @@ const Page404 = () => {
   return (
     <>
       <Head>
-        <title>Page not found - AZ Software</title>
+        <title>Page not found</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="page__wrapper" style={{ height: '100%' }}>
         <Page>
           <Text h1>404.</Text>
           <Text h4>This page does not exist.</Text>
-          <Note width="50%" type="warning" label="Tip">
+          <Note style={{display:"inline-block"}} type="warning" label="Tip">
             Join in and{' '}
             <Link underline color href="https://github.com/avdan-os">
               help us
             </Link>{' '}
-            out developing <strong>open-source software</strong>.
+            out developing an <strong>open-source operating system</strong>.
           </Note>
         </Page>
-      </div>
     </>
   );
 };
