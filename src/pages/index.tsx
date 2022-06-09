5cd64b21ca679bb692979129b5e30916cb283778
AZProductions <66299945+AZProductions@users.noreply.github.com>
2022-06-09 18:42:37 +0200

    chore: added animated banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,10 @@ import dynamicWidth from '@/lib/dynamic-width';
 import { useState } from 'react';
 
 const index = () => {
-  const [useMobileBar, setMobileBar] = useState(false)
-  dynamicWidth(width => {
-    (width < 1200) ? setMobileBar(true) : setMobileBar(false);
-  })
+  const [useMobileBar, setMobileBar] = useState(false);
+  dynamicWidth((width) => {
+    width < 1200 ? setMobileBar(true) : setMobileBar(false);
+  });
   return (
     <>
       <div className="text-center">
@@ -16,18 +16,23 @@ const index = () => {
         </Text>
         <div className="mx-auto" style={{ width: '80%', height: '100%' }}>
           <Card shadow>
-            <Image draggable="false" src="/assets/marketing/header.png" height="100%" width="100%" />
+            {/* <Image draggable="false" src="/assets/marketing/header.png" height="100%" width="100%" /> */}
+            <video controls autoPlay={true} muted loop style={{ width: '100%', height: '100%' }}>
+              <source type="video/webm" src="/assets/marketing/header_animated.webm" height="100%" width="100%" />
+            </video>
           </Card>
         </div>
       </div>
       <Spacer h={2} />
       <div className="center card-container">
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-          {useMobileBar ? null : (<NextLink href="/demo">
-            <Button shadow type="secondary" margin="10px">
-              Try in your browser
-            </Button>
-          </NextLink>)}
+          {useMobileBar ? null : (
+            <NextLink href="/demo">
+              <Button shadow type="secondary" margin="10px">
+                Try in your browser
+              </Button>
+            </NextLink>
+          )}
           <NextLink href="/downloads">
             <Button shadow type="success" margin="10px">
               Download Now
