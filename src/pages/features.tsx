d78f4a3dbbe81ab7a442628e1c6b9d14908684f8
Astroneural <105980825+Astroneural@users.noreply.github.com>
2022-06-14 15:10:32 -0500

    Update features.tsx

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -6,25 +6,25 @@ const features = () => {
   return (
     <>
       <FeaturesCard
-        cardTitle="A new way to manage your files"
-        cardDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ad fugiat nostrum autem, est veritatis adipisci eum provident quia minus minima tenetur pariatur. Quidem laudantium iure magnam quam mollitia quos."
+        cardTitle="A New Way to Manage Your Files"
+        cardDescription="With the improved File Manager, you can easily navigate your files with tabs, pinned folders, and tags, all in a beautiful UI with modern but recognizable icons inspired by Avdan's designs."
         cardVideo="/assets/marketing/header_animated.webm"
         isInverted={false}
       />
       <FeaturesCard
-        cardTitle="Next level drag and drop experience "
+        cardTitle="Next Level Drag and Drop Experience "
         cardDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ad fugiat nostrum autem, est veritatis adipisci eum provident quia minus minima tenetur pariatur. Quidem laudantium iure magnam quam mollitia quos."
         cardVideo="/assets/marketing/header_animated.webm"
         isInverted={true}
       />
       <FeaturesCard
-        cardTitle="Personalize the way you want"
-        cardDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ad fugiat nostrum autem, est veritatis adipisci eum provident quia minus minima tenetur pariatur. Quidem laudantium iure magnam quam mollitia quos."
+        cardTitle="Personalize the Way You Want"
+        cardDescription="Avdan OS is an open-source operating system based on Arch Linux. For developers, this means easy and limitless customizability for everything. For regular users, Avdan OS is built in with different presets and allows for free customization using Settings."
         cardVideo="/assets/marketing/header_animated.webm"
         isInverted={false}
       />
       <FeaturesCard
-        cardTitle="Limitless possibilities for multitasking"
+        cardTitle="Limitless Possibilities for Multitasking"
         cardDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ad fugiat nostrum autem, est veritatis adipisci eum provident quia minus minima tenetur pariatur. Quidem laudantium iure magnam quam mollitia quos."
         cardVideo="/assets/marketing/header_animated.webm"
         isInverted={true}
