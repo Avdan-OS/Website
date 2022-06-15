ec64c8ca87cd6ec1617662994b64f5890d3d7177
AZ Software <66299945+AZProductions@users.noreply.github.com>
2022-06-15 15:50:55 +0200

    chore: changed to new demo

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -32,7 +32,7 @@ const preview = () => {
             style={{ padding: '0', width: '100%', height: '45rem ' }}
             height="100%"
             width="100%"
-            src="https://dynamicos.netlify.app/"
+            src="https://demo.avdanos.com"
           />
           <div style={{ display: 'flex', margin: '15px' }}>
             <Button shadow type="success" margin="auto" onClick={fullScreen}>
