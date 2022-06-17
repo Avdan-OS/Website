d29d241de224e2d3073b454ed515b228ce99a814
Froxcey <danichen204@gmail.com>
2022-06-17 23:09:45 +0800

    Fix build fail

--- src/components/WidthRequirement.tsx
+++ src/components/WidthRequirement.tsx
@@ -10,6 +10,12 @@ import NextLink from 'next/link';
 import { useState } from 'react';
 import dynamicWidth from '@/lib/dynamic-width';
 
+declare global {
+  interface Window {
+    opera: any;
+  }
+}
+
 const WidthRequirement = ({ width }) => {
   const { setVisible, bindings } = useModal();
   const [subtitle, setSubtitle] = useState('Rotate Your Screen');
