f125c019cf00e8618edbadcbae920e6814c7e69f
nicosammito <kynxyd-bUxnek-9vohsi>
2022-06-22 02:37:11 +0200

    alert is now 100% on all other pages except index

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,11 +1,12 @@
 import { Link, Grid, Card } from '@geist-ui/core';
 import style from '../styles/components/cards.module.css';
+import {useRouter} from "next/router";
 
 const Alert = () => {
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Grid style={{ paddingInline: 50, pointerEvents: 'none', display: 'block', width: '60vw' }}>
+        <Grid style={{ paddingInline: 50, pointerEvents: 'none', display: 'block', width: (useRouter().route == "/" ? '60vw' : "100%") }}>
           <Card className={style.alert__card}>
             <p style={{ fontSize: 14, margin: -10, marginLeft: 0 }}>
               <strong>
