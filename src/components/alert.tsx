6bd1965e71a89885f74a1d001cf42a19adde4fbd
nicosammito <kynxyd-bUxnek-9vohsi>
2022-06-22 03:21:02 +0200

    white mode is now working on alert

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -8,7 +8,7 @@ const Alert = () => {
     <>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Grid style={{ paddingInline: 50, pointerEvents: 'none', display: 'block', width: (useRouter().route == "/" ? '60vw' : "100%") }}>
-          <Card className={style.card + " " + (theme.type == "dark" ? style.card__dark: style.card__dark)}>
+          <Card className={style.card + " " + (theme.type == "dark" ? style.card__dark: style.card__white)}>
             <p style={{ fontSize: 14, margin: -10, marginLeft: 0 }}>
               <strong>
                 <strong style={{ fontWeight: 400 }}>AvdanOS</strong> is still in development.
