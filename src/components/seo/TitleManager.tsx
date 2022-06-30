512a185c0943cfcff19c2969947aa4eaddeda890
AZProductions <66299945+AZProductions@users.noreply.github.com>
2022-06-30 13:29:16 +0200

    chore: added useScale function to titlemanager

--- src/components/seo/TitleManager.tsx
+++ src/components/seo/TitleManager.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
+import { TranslatableText } from '../translation/TranslatableText';
 interface TitleManagerProps {
   title: string;
+  useScale: boolean;
 }
 
-const TitleManager = ({ title }: TitleManagerProps) => {
+const TitleManager = ({ title, useScale }: TitleManagerProps) => {
   return (
     <Head>
-      <title>{title + ' - AvdanOS'}</title>
+      <title>
+        <TranslatableText>{title + ' - AvdanOS'}</TranslatableText>
+        {useScale ? <meta name="viewport" content="initial-scale=1.0, width=device-width" /> : null}
+      </title>
     </Head>
   );
 };
