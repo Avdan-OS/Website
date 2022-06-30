82bb409476f26d4c4e0c61113dc3bbc200f0e84e
AZProductions <66299945+AZProductions@users.noreply.github.com>
2022-06-30 13:31:49 +0200

    chore: added description prop

--- src/components/seo/TitleManager.tsx
+++ src/components/seo/TitleManager.tsx
@@ -4,14 +4,17 @@ import { TranslatableText } from '../translation/TranslatableText';
 interface TitleManagerProps {
   title: string;
   useScale: boolean;
+  description: string;
 }
 
-const TitleManager = ({ title, useScale }: TitleManagerProps) => {
+const TitleManager = ({ title, useScale, description }: TitleManagerProps) => {
   return (
     <Head>
       <title>
         <TranslatableText>{title + ' - AvdanOS'}</TranslatableText>
         {useScale ? <meta name="viewport" content="initial-scale=1.0, width=device-width" /> : null}
+        <meta name="twitter:title" content={description + ' - AvdanOS.com'} />
+        <meta name="description" content={description + ' - AvdanOS.com'} />
       </title>
     </Head>
   );
