7e2df90e616cf3839478404f9de3d2d05a39be2a
iDev <88238063+iDevYT@users.noreply.github.com>
2022-07-04 19:51:14 +0000

    documents

--- src/components/navigation/menu.tsx
+++ src/components/navigation/menu.tsx
@@ -51,7 +51,7 @@ const Menu = () => {
               />
               <Tabs.Item label={<TranslatableText>Downloads</TranslatableText>} value="/downloads" />
               <Tabs.Item label={<TranslatableText>Support</TranslatableText>} value="/support" />
-              <Tabs.Item label={<TranslatableText>Docs</TranslatableText>} value="/docs" />
+              <Tabs.Item label={<TranslatableText>Documents</TranslatableText>} value="/docs" />
             </Tabs>
           </ul>
         </div>
