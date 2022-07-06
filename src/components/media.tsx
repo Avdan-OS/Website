88bea7bb82e8c9df23e6a956a6172dd8096c66a9
GG Gaming <106931950+GGGaming71@users.noreply.github.com>
2022-07-06 12:02:31 +0300

    Updated media.tsx

--- src/components/media.tsx
+++ src/components/media.tsx
@@ -1,7 +1,7 @@
 import { Grid, Card } from '@geist-ui/core';
-import { RiTwitterFill } from 'react-icons/ri';
+import { RiInstagramFill, RiTwitterFill } from 'react-icons/ri';
 import { AiFillYoutube, AiFillGithub } from 'react-icons/ai';
-import { FaRedditAlien, FaDiscord } from 'react-icons/fa';
+import { FaRedditAlien, FaDiscord, FaInstagram } from 'react-icons/fa';
 
 const Media = () => {
   return (
@@ -21,14 +21,14 @@ const Media = () => {
         </a>
       </Grid>
       <Grid>
-        <a href="https://www.youtube.com/c/AvdanOSDeveloper" target="_blank" rel="noreferrer">
+        <a href="https://www.youtube.com/channel/UCHLCBj83J7bR82HwjhCJusA" target="_blank" rel="noreferrer">
           <Card shadow>
             <AiFillYoutube size={26} />
           </Card>
         </a>
       </Grid>
       <Grid>
-        <a href="https://discord.gg/gRCcCUZ5px" target="_blank" rel="noreferrer">
+        <a href="https://discord.gg/avdanos" target="_blank" rel="noreferrer">
           <Card shadow>
             <FaDiscord size={26} />
           </Card>
@@ -38,6 +38,13 @@ const Media = () => {
         <a href="https://www.reddit.com/r/AvdanOS/" target="_blank" rel="noreferrer">
           <Card shadow>
             <FaRedditAlien size={26} />
+          </Card>
+        </a>
+      </Grid>
+      <Grid>
+        <a href="https://www.instagram.com/avdanos_" target="_blank" rel="noreferrer">
+          <Card shadow>
+            <FaInstagram size={26} />
           </Card>
         </a>
       </Grid>
