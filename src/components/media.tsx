613b54998b72d52bf3699fb64460eeb5034baa50
GG Gaming <106931950+GGGaming71@users.noreply.github.com>
2022-07-06 11:56:57 +0300

    Add files via upload

---
+++ src/components/media.tsx
@@ -0,0 +1,48 @@
+import { Grid, Card } from '@geist-ui/core';
+import { RiTwitterFill } from 'react-icons/ri';
+import { AiFillYoutube, AiFillGithub } from 'react-icons/ai';
+import { FaRedditAlien, FaDiscord } from 'react-icons/fa';
+
+const Media = () => {
+  return (
+    <Grid.Container gap={1} justify="center">
+      <Grid>
+        <a href="https://twitter.com/avdan_os" target="_blank" rel="noreferrer">
+          <Card shadow>
+            <RiTwitterFill size={26} />
+          </Card>
+        </a>
+      </Grid>
+      <Grid>
+        <a href="https://github.com/Avdan-OS/" target="_blank" rel="noreferrer">
+          <Card shadow>
+            <AiFillGithub size={26} />
+          </Card>
+        </a>
+      </Grid>
+      <Grid>
+        <a href="https://www.youtube.com/c/AvdanOSDeveloper" target="_blank" rel="noreferrer">
+          <Card shadow>
+            <AiFillYoutube size={26} />
+          </Card>
+        </a>
+      </Grid>
+      <Grid>
+        <a href="https://discord.gg/gRCcCUZ5px" target="_blank" rel="noreferrer">
+          <Card shadow>
+            <FaDiscord size={26} />
+          </Card>
+        </a>
+      </Grid>
+      <Grid>
+        <a href="https://www.reddit.com/r/AvdanOS/" target="_blank" rel="noreferrer">
+          <Card shadow>
+            <FaRedditAlien size={26} />
+          </Card>
+        </a>
+      </Grid>
+    </Grid.Container>
+  );
+};
+
+export default Media;
