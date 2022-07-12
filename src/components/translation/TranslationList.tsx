e81f49f2c9d3423ffb05a5dfb737cfe43eda4716
Froxcey <danichen204@gmail.com>
2022-07-12 11:24:23 +0800

    Add Uzbek

--- src/components/translation/TranslationList.tsx
+++ src/components/translation/TranslationList.tsx
@@ -101,6 +101,12 @@ const TranslationList = () => {
       langName: 'Swedish'
     },
     {
+      lang: 'uz_UZ',
+      available: true,
+      icon: '🇺🇿',
+      langName: 'Uzbek'
+    },
+    {
       lang: 'zh_CN',
       available: true,
       icon: '🇨🇳',
