bb4abdd25cf7a683bd1b45fcec22c45cc61ceb3b
Padmanabh <kharat82@gmail.com>
2022-07-22 17:40:37 +0530

    Installed and Ran Prettier

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -4,54 +4,55 @@ import { useRouter } from 'next/router';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 const FAQ = ({ YamlData }) => {
+  const Path = useRouter().asPath;
 
-    const Path = useRouter().asPath
+  type Question = {
+    Title: string;
+    SubTitle: string;
+    Answer: string;
+    QuestionNumber: any;
+  };
 
-    type Question = {
-        Title: string;
-        SubTitle: string;
-        Answer: string;
-        QuestionNumber: any
-    };
+  type CategoryType = {
+    Name: String;
+    Questions: Array<Question>;
+  };
 
-    type CategoryType = {
-        Name: String;
-        Questions: Array<Question>;
-    };
-
-    return (
-        <div id="faq">
-            <Collapse.Group>
-                {YamlData.FAQs.map((Category: CategoryType, index: any) => (
-                    <div className='Category' key={index}>
-                        <div className='CategoryName' >{Category.Name}</div>
-                        {Category.Questions.map((Question: Question) => (
-                            <Collapse
-                                className="Questions"
-                                style={{ overflowWrap: 'anywhere' }}
-                                id={Question.QuestionNumber}
-                                key={Question.QuestionNumber}
-                                subtitle={Question.SubTitle}
-                                title={Question.Title}
-                                initialVisible={Path.endsWith(`#${Question.QuestionNumber}`)}
-                            >
-                                <Text><ReactMarkdown>{Question.Answer}</ReactMarkdown></Text>
-                            </Collapse>
-                        ))}
-                    </div>
-                ))}
-            </Collapse.Group>
-        </div>
-    );
+  return (
+    <div id="faq">
+      <Collapse.Group>
+        {YamlData.FAQs.map((Category: CategoryType, index: any) => (
+          <div className="Category" key={index}>
+            <div className="CategoryName">{Category.Name}</div>
+            {Category.Questions.map((Question: Question) => (
+              <Collapse
+                className="Questions"
+                style={{ overflowWrap: 'anywhere' }}
+                id={Question.QuestionNumber}
+                key={Question.QuestionNumber}
+                subtitle={Question.SubTitle}
+                title={Question.Title}
+                initialVisible={Path.endsWith(`#${Question.QuestionNumber}`)}
+              >
+                <Text>
+                  <ReactMarkdown>{Question.Answer}</ReactMarkdown>
+                </Text>
+              </Collapse>
+            ))}
+          </div>
+        ))}
+      </Collapse.Group>
+    </div>
+  );
 };
 
 export async function getStaticProps() {
-    const YamlData = await YAML.parse(await (await fetch('http://localhost:3000/assets/FAQ.yaml')).text());
-    return {
-        props: {
-            YamlData
-        }
-    };
+  const YamlData = await YAML.parse(await (await fetch('http://localhost:3000/assets/FAQ.yaml')).text());
+  return {
+    props: {
+      YamlData
+    }
+  };
 }
 
 export default FAQ;
