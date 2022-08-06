3e47fca65f17761d651b5f54460ce7fef9fec3cd
Padmanabh Kharat <70370670+Padmanabh82@users.noreply.github.com>
2022-08-06 19:10:03 +0530

    Update faq.tsx

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -3,22 +3,20 @@ import { useRouter } from 'next/router';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 const FAQ = () => {
-
-  const FAQquestions = {
-    FAQs: [
-      {
-        Name: "AvdanOS",
-        Questions: [
-          {
-            QuestionNumber: 1,
-            Title: "What will AvdanOS be?",
-            SubTitle: "",
-            Answer: "We are simply doing a Desktop Environment. But don’t get too mad, we don’t need more than that. We do not need to code or package a whole OS.\n\n> **Linux package =** “package” refers to a **compressed file archive containing all of the files that come with a particular application**. The files are usually stored in the package according to their relative installation paths on your system. Most packages also contain installation instructions for the OS, as well as a list of any other packages that are dependencies (prerequisites required for installation.\n> \n\n🧠 This means that we will need to simply just make a desktop environment and package it. Linux will figure it out.\n\nThere is a **huge misconception that the UI you see when you use a particular OS is the OS itself**. It is not. Those are 2 completely different things. The **OS** is what *screws around with all the hardware* you have (i.e. the Kernel and the System Programs). The **applications** (Application Programs to be precise) that you use, including the desktop UI *is not a part of the operating system*.\n\n🧠 We will also write a **Wayland Compositor [The Wayland Compositor ](https://www.notion.so/The-Wayland-Compositor-d50267f916ae4592bfe9a3340e5472f1)** to allow for the custom features and behaviour we want to take place. It will be made using **Rust** and [**Smithay**](https://smithay.github.io/) to add additional Features to the Desktop Environment.\n"
-          }
-        ]
-      }
-    ]
-  }
+  const FAQ = [
+    {
+      Name: 'AvdanOS',
+      Questions: [
+        {
+          QuestionNumber: 1,
+          Title: 'What will AvdanOS be?',
+          SubTitle: '',
+          Answer:
+            'We are simply doing a Desktop Environment. But don’t get too mad, we don’t need more than that. We do not need to code or package a whole OS.\n\n> **Linux package =** “package” refers to a **compressed file archive containing all of the files that come with a particular application**. The files are usually stored in the package according to their relative installation paths on your system. Most packages also contain installation instructions for the OS, as well as a list of any other packages that are dependencies (prerequisites required for installation.\n> \n\n🧠 This means that we will need to simply just make a desktop environment and package it. Linux will figure it out.\n\nThere is a **huge misconception that the UI you see when you use a particular OS is the OS itself**. It is not. Those are 2 completely different things. The **OS** is what *screws around with all the hardware* you have (i.e. the Kernel and the System Programs). The **applications** (Application Programs to be precise) that you use, including the desktop UI *is not a part of the operating system*.\n\n🧠 We will also write a **Wayland Compositor [The Wayland Compositor ](https://www.notion.so/The-Wayland-Compositor-d50267f916ae4592bfe9a3340e5472f1)** to allow for the custom features and behaviour we want to take place. It will be made using **Rust** and [**Smithay**](https://smithay.github.io/) to add additional Features to the Desktop Environment.\n'
+        }
+      ]
+    }
+  ];
 
   const Path = useRouter().asPath;
 
@@ -37,12 +35,12 @@ const FAQ = () => {
   return (
     <div id="faq">
       <Collapse.Group>
-        {FAQquestions.FAQs.map((Category: CategoryType, index: any) => (
-          <div className="Category" key={index}>
-            <div className="CategoryName">{Category.Name}</div>
+        {FAQ.map((Category: CategoryType, index: any) => (
+          <div className="category" key={index}>
+            <div className="categoryName">{Category.Name}</div>
             {Category.Questions.map((Question: Question) => (
               <Collapse
-                className="Questions"
+                className="questions"
                 style={{ overflowWrap: 'anywhere' }}
                 id={Question.QuestionNumber}
                 key={Question.QuestionNumber}
@@ -63,3 +61,4 @@ const FAQ = () => {
 };
 
 export default FAQ;
+
