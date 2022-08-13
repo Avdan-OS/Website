fcf2104acfbc41c187f81e4cf0dea1495c8d2d34
Froxcey <danichen204@gmail.com>
2022-08-14 00:48:21 +0800

    New translation & convention patch
    - Update text to en_GB by default
    - Added new translation to files

--- src/components/miniFeatures.tsx
+++ src/components/miniFeatures.tsx
@@ -3,11 +3,8 @@ import NextLink from 'next/link';
 import { TranslatableText } from '@/components/translation/TranslatableText';
 import SlideshowExplainer from '@/components/slideshowExplainer';
 
-
-
 // Creates small presentation of AvdanOS features called `miniFeatures()`
 const MiniFeatures = () => {
-
   return (
     <>
       <div className="center">
@@ -17,29 +14,27 @@ const MiniFeatures = () => {
 
         <Text p className="text-center" style={{ fontSize: '1.2rem', padding: '0 30px' }}>
           <TranslatableText>
-            Time is priceless, and there is so little time.
-            With right tools and professional workspace, 
-            every task is simple as pie for the least amount of time!
+            Time is priceless, and there is so little time. With right tools and professional workspace, every task is
+            simple as pie for the least amount of time!
           </TranslatableText>
         </Text>
 
-        <Spacer h="15px"/>
+        <Spacer h="15px" />
         <SlideshowExplainer />
-        
-        <Spacer h="15px"/>
+
+        <Spacer h="15px" />
         <div className="text-center">
           <Text p className="text-center" style={{ fontSize: '1.2rem' }}>
             <TranslatableText>Check what AvdanOS offers to you.</TranslatableText>
           </Text>
 
-          <NextLink href="/features-beta" >
+          <NextLink href="/features-beta">
             <Button shadow type="success" color="primary">
               <TranslatableText>See here</TranslatableText>
             </Button>
           </NextLink>
         </div>
       </div>
-        
     </>
   );
 };
